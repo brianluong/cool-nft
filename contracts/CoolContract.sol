04936df3a40e28b28818a14f7e2a82ffe09894ce
Brian Luong <brian@satsuma.xyz>
2022-11-10 11:03:59 -0600

    lol

--- contracts/CoolContract.sol
+++ contracts/CoolContract.sol
@@ -2,7 +2,7 @@
 // Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
 pragma solidity ^0.7.0;
 
-contract DulceNFT {
+contract CoolContract {
 
   string public message;
 
